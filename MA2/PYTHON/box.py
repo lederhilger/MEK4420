5974dfff9ec26fe2f24ab36835b23fc7d576c0fe
simonled <simon.lederhilger@gmail.com>
2025-03-17 08:51:02 +0100

    Added Box class

--- MA2/PYTHON/box.py
+++ MA2/PYTHON/box.py
@@ -1,14 +1,23 @@
 from numpy import array, zeros, linspace
-def box(Nx: int, Ny: int, L: float, D: float) -> array:
-    x = linspace(-L/2, L/2, Nx)
-    y = linspace(0, -D, Ny)
-    box = zeros((Nx + 2*Ny, 2))
-    for n in range(Ny):
-        box[n] = [x[0],y[n]]
-        box[n + Nx + Ny] = [x[-1], y[-n-1]]
-    for n in range(Nx):
-        box[n + Ny] = [x[n], y[-1]]
-    return box
+class Box:
+    def __init__(self, Nx: int, Ny: int, L: float, D: float):
+        self.Nx = Nx
+        self.Ny = Ny
+        self.L = L
+        self.D = D
+
+    
+
+    def box(self) -> array:
+        x = linspace(-self.L/2, self.L/2, self.Nx)
+        y = linspace(0, -self.D, self.Ny)
+        box = zeros((self.Nx + 2*self.Ny, 2))
+        for n in range(self.Ny):
+            box[n] = [x[0],y[n]]
+            box[n + self.Nx + self.Ny] = [x[-1], y[-n-1]]
+        for n in range(self.Nx):
+            box[n + self.Ny] = [x[n], y[-1]]
+        return box
 
 box = box(10,10,2,1).T
 print (box)
