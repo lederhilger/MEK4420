3c33119a938e9b1f27e864092d11185d4d1014c5
simonled <simon.lederhilger@gmail.com>
2025-03-17 15:54:26 +0100

    LHS for phi_k implemented

--- MA2/PYTHON/solve/integralequation.py
+++ MA2/PYTHON/solve/integralequation.py
@@ -1,4 +1,5 @@
-from numpy import array, zeros_like, zeros, sqrt
+from numpy import array, zeros_like, zeros, sqrt, pi, arccos
+from scipy import special, exp, log
 from solve.box import Box
 
 class IntegralEquation(Box):
@@ -8,6 +9,7 @@ class IntegralEquation(Box):
         self.xm = super(IntegralEquation, self).x_m().T
         self.ж = super(IntegralEquation, self).box().T
         self.N = len(self.ж[0])
+        self.κ = 1.2/self.D
 
     def Δx(self) -> array:
         Δx = zeros(self.N); Δy = zeros(self.N)
@@ -35,3 +37,43 @@ class IntegralEquation(Box):
         for n in range(self.N):
             dS[n] = sqrt(δx[n]**2 + δy[n]**2)
         return dS
+
+    def f_1(self, z):
+        f_1 = -2*exp(z) * (special.exp1(z) + log(z) - log(-z))
+        return f_1
+    
+    def f_2(self, z):
+        f_2 = 2*pi*exp(z)
+        return f_2
+
+    def lhs_k(self) -> array:
+        '''
+            Assembles the left-hand side of the integral equation
+            -pi phi_k + int_{S_{mathrm{B}}} phi_k partial_{nhat} green ,dee S
+            = int_{S_{mathrm{B}}} green hat{n}_k ,dee S
+            This is equation 101
+        '''
+        x_p, y_p = self.xp
+        x_m, y_m = self.xm
+        ж, ч = self.ж
+        nx, ny = self.normal_vector()
+        dS = self.dS()
+        dΘ = zeros((self.N,self.N), dtype = 'complex_')
+        tol = 1e-8
+        for n in range(self.N):
+            for m in range(self.N):
+                a_x = x_m[m] - ж[n]
+                a_y = y_m[m] - ч[n]
+                b_x = x_p[m] - ж[n]
+                b_y = y_p[m] - ч[n]
+                if n == m:
+                    argument_1 = -pi
+                else:
+                    argument_1 = log((a_x + 1j*a_y)/(b_x + 1j*b_y)).imag
+                c_1 = y_m[m] - ч[n]
+                c_2 = y_p[m] - ч[n]
+                argument_2 = log((a_x + 1j*c_1)/(b_x + 1j*c_2))
+                з = self.κ*(ч[m] + ч[n] - 1j*(ж[m] - ж[n]))
+                argument_3 = (nx[m]*(self.f_1(з).imag + 1j*(self.f_2(з).imag)) + ny[m]*(self.f_1(з).real + 1j*(self.f_2(з).real)))*self.κ*dS[m]
+                dΘ[n,m] = argument_1 + argument_2 + argument_3
+        return dΘ
--- MA2/PYTHON/test.py
+++ MA2/PYTHON/test.py
@@ -1,6 +1,6 @@
 from solve.integralequation import IntegralEquation
 from numpy import *
-box = IntegralEquation(10,10, 2, 1)
-zhe = box.dS(); print(zhe)
+box = IntegralEquation(5,5, 2, 1)
+zhe = box.lhs_k(); print(zhe)
 #import matplotlib.pyplot as plt
 #plt.plot(zhe[0], zhe[1], '*'); plt.show()
