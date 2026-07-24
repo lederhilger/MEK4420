9f45d83b843aad510e5160fd278d6c781d6de3c9
simonled <simon.lederhilger@gmail.com>
2026-07-24 23:28:32 +0200

    Fixed normal vector orientation; vectorizing assembly; caching values

--- MA1/python/solve/integralequation.py
+++ MA1/python/solve/integralequation.py
@@ -1,8 +1,9 @@
-from numpy import (ndarray, linalg, sqrt, pi,
-                    zeros, angle, linspace, trapz, diff, fill_diagonal)
+from numpy import (ndarray, linalg, sqrt, pi, angle,
+                    zeros, linspace, diff, fill_diagonal, column_stack)
 from solve.chebyshov import Chebyshov
 from solve.quadrature import Quadrature
 from solve.potentials import Potentials
+from functools import cached_property
 
 class IntegralEquation:
     def __init__(self, N, coordinates, order):
@@ -12,78 +13,66 @@ class IntegralEquation:
         self.ж = zeros((2,self.N))
         self.ж[0] = .5*(self.x[1:]+self.x[:-1])
         self.ж[1] = .5*(self.z[1:]+self.z[:-1])
-        # for n in range(self.N):
-        #     ж[0,n], ж[1,n] = .5*(self.x[n+1]+self.x[n]), .5*(self.z[n+1]+self.z[n])
-        # self.ж = ж
         self.order = order
-    
+
+    @cached_property
     def Δx(self) -> ndarray:
         Δx = diff(self.x)
         Δz = diff(self.z)
         return Δx, Δz
 
+    @cached_property
     def dS(self) -> ndarray:
-        δx, δz = self.Δx()
+        δx, δz = self.Δx
         dS = sqrt(δx**2 + δz**2)
         return dS
 
+    @cached_property
     def normal_vector(self) -> ndarray:
         '''
         Δx = x_m+1 - x_m, Δz = z_m+1 - z_m
-        nhat = (-Δz, Δx)
+        nhat = (Δz, -Δx)
         '''
-        δx, δz = self.Δx()
-        denominator = self.dS()
-        nx = -δz/denominator
-        nz = δx/denominator
+        δx, δz = self.Δx
+        denominator = self.dS
+        nx = δz/denominator
+        nz = -δx/denominator
         return nx, nz
     
     def assemble(self) -> ndarray:
-        ж, ч = self.ж
-        dΘ = zeros((self.N,self.N))
-        for i in range(self.N):
-            for j in range(self.N):
-                if i == j:
-                    dΘ[i,j] = -pi
-                else:
-                    dΘ[i,j] = -angle(complex(self.x[j+1]-ж[i], self.z[j+1]-ч[i])/complex(self.x[j]-ж[i], self.z[j]-ч[i]))
-                    # dΘ[i,j] = arctan2((self.z[j]-ч[i]), (self.x[j]-ж[i])) - arctan2((self.z[j+1]-ч[i]), (self.x[j+1]-ж[i]))
+        Ж = self.ж[0] + 1j*self.ж[1]
+        X = self.x + 1j*self.z
+        start = X[:-1]; end = X[1:]
+        numerator = end[None,:] - Ж[:,None]
+        denominator = start[None,:] - Ж[:,None]
+        dΘ = -angle(numerator/denominator)
+        fill_diagonal(dΘ, -pi)
         return dΘ
     
     def assemble_h(self) -> ndarray:
-        δx = self.Δx()
+        δx = self.Δx
         ж = self.ж
         h = Quadrature(self.N, δx, ж, 'Lagrange', self.order).quad()
         return h
     
-    def right_hs(self, assemble_h, mode: int) -> ndarray:
-        n_x, n_z = self.normal_vector()
+    def right_hs(self, assemble_h) -> ndarray:
+        n_x, n_z = self.normal_vector
         ж, ч = self.ж
-        if mode == 1:
-            n_i = n_x
-        elif mode == 2:
-            n_i = n_z
-        elif mode == 6:
-            n_i = zeros(self.N)
-            for n in range(self.N):
-                n_i[n] = ж[n]*n_z[n] - ч[n]*n_x[n]
-        else:
-            raise ValueError("Choose mode: 1, 2, 6")
-        right_hs = assemble_h @ n_i
+        modes = column_stack((n_x, n_z, ж*n_z - ч*n_x))
+        right_hs = assemble_h @ modes
         return right_hs
     
     def solve(self):
         assemble = self.assemble()
         h = self.assemble_h()
-        phi_1 = linalg.solve(assemble, self.right_hs(h, 1))
-        phi_2 = linalg.solve(assemble, self.right_hs(h, 2))
-        phi_6 = linalg.solve(assemble, self.right_hs(h, 6))
-        return phi_1, phi_2, phi_6
+        right_hs = self.right_hs(h)
+        phi = linalg.solve(assemble, right_hs)
+        return phi[:,0], phi[:,1], phi[:,2]
 
     def added_mass(self, phi):
         m_11 = 0; m_22 = 0; m_66 = 0
         phi_1, phi_2, phi_6 = phi
-        nx, nz = self.normal_vector(); dS = self.dS()
+        nx, nz = self.normal_vector; dS = self.dS
         ж, ч = self.ж
         m_11 = (phi_1*nx*dS).sum()
         m_22 = (phi_2*nz*dS).sum()
@@ -92,9 +81,10 @@ class IntegralEquation:
 
     def normal_plot(self):
         import matplotlib.pyplot as plt
-        nx, nz = self.normal_vector(); ж, ч = self.ж()
-        for n in range(self.N):
-            plt.plot((ж[n], nx[n]+ж[n]), (ч[n], nz[n]+ч[n]))
-            plt.plot((self.x[n+1], self.x[n]), (self.z[n+1], self.z[n]))
-        plt.plot(ж, ч, 'x')
+        nx, nz = self.normal_vector; ж, ч = self.ж
+        fig, ax = plt.subplots()
+        ax.plot(self.x, self.z, color='k')
+        ax.scatter(ж, ч, marker='x')
+        ax.quiver(ж, ч, nx, nz, angles = 'xy', scale_units = 'xy', scale = 1)
+        ax.set_aspect('equal')
         plt.show()
